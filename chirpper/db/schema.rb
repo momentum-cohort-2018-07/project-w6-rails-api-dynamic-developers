539f794d039eae1239db94e809efb8fe6b223610
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-09-08 13:05:59 -0400

    Deleted dup table

--- chirpper/db/schema.rb
+++ chirpper/db/schema.rb
@@ -10,15 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_09_07_200225) do
-
 ActiveRecord::Schema.define(version: 2018_09_07_203604) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.bigint "post_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "follows", force: :cascade do |t|
     t.string "followable_type", null: false
@@ -32,15 +37,6 @@ ActiveRecord::Schema.define(version: 2018_09_07_203604) do
     t.index ["followable_type", "followable_id"], name: "index_follows_on_followable_type_and_followable_id"
     t.index ["follower_id", "follower_type"], name: "fk_follows"
     t.index ["follower_type", "follower_id"], name: "index_follows_on_follower_type_and_follower_id"
-
-  create_table "comments", force: :cascade do |t|
-    t.string "body"
-    t.bigint "post_id"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -63,5 +59,4 @@ ActiveRecord::Schema.define(version: 2018_09_07_203604) do
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
-
 end
