e5f5d23e985c34586260a160ff34083e931d989d
Stephen Patterson <swpat23@gmail.com>
2018-09-08 12:35:34 -0400

    built out post and comment models and controllers, started doc

--- chirpper/app/controllers/comments_controller.rb
+++ chirpper/app/controllers/comments_controller.rb
@@ -1,2 +1,38 @@
 class CommentsController < ApplicationController
+
+  def index
+    @post = Post.find(params[:post_id])
+    render json: @post.comments
+  end
+
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id]) 
+    render json: @Comment
+  end
+ 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy  
+    render json: @comment
+  end
+ 
+  private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+
+
 end
--- chirpper/app/controllers/posts_controller.rb
+++ chirpper/app/controllers/posts_controller.rb
@@ -1,2 +1,41 @@
 class PostsController < ApplicationController
+
+  # GET /post
+  def index
+    @post = Post.all
+    render json: @post
+  end
+  # GET /post/post_id
+  def show
+    @post = Post.find(params[:id])
+      render json: @post
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post, status: :created, location: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/1
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    render json: @post
+  end
+
+  private
+  
+  # Only allow a trusted parameter "white list" through.
+    def post_params
+      params.require(:post).permit(:body)
+    end
+
 end
+
+
